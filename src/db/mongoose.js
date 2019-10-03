74210d2bd7386c9bc5e0d40144e17552771fa252
Harshit Batra <payafterwork.in@gmail.com>
2019-10-03 15:20:55 +0530

    /user for Rest API intalling nodemon for dev,express

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,46 +1,19 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
         required: true,
-        trim: true 
+        trim: true
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true, 
-        validate(value) {
-            if (!validator.isEmail(value)) { //using validator package
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0, 
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a postive number')
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false
     }
-})
-
-const me = new User({
-    name: '   Andrew  ',
-    email: 'MYEMAIL@MEAD.IO   '
-})
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
 })
