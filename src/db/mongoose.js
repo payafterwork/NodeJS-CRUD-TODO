24b3c0c315e14c4f3b8c4a3d57380437309ed2e2
Harshit Batra <payafterwork.in@gmail.com>
2019-10-03 12:35:48 +0530

    Data validation, santization with validator, mongoose package

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,29 +1,46 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1:27017/mongoose-db',{
- useNewUrlParser: true,
- useCreateIndex: true,
- useUnifiedTopology: true 
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
 
-const User = mongoose.model('user',{
-   name: {
-     type: String
-   },
-   age: {
-     type: Number
-   }
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true 
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true, 
+        validate(value) {
+            if (!validator.isEmail(value)) { //using validator package
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0, 
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    }
 })
-
 
 const me = new User({
-    name: 'Harshit',
-    age: 27
+    name: '   Andrew  ',
+    email: 'MYEMAIL@MEAD.IO   '
 })
 
-me.save().then(
-  ()=>{
-      console.log(me);
-  }).catch((error)=>{
-      console.log('Error!', error);
-  })
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
