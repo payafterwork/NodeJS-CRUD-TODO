131465f73e5c6fd588be97aaa13d42e32fd05aed
Harshit Batra <payafterwork.in@gmail.com>
2019-10-08 12:08:22 +0530

    JWT auth from header not working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express =  require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -10,6 +10,18 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+//Middleware between: new request > (middleware) > run route handler
+app.use((req,res,next)=>{
+    if(req.method == 'GET'){
+    }else{
+     next()
+    }
+})
+
+//Maintainance Mode Middleware
+// app.use((req,res,next)=>{
+//     res.status(503).status('Site is currently down. Check back soon!')
+// })
 
 app.listen(port,()=>{
     console.log('Server is up on port'+port);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,97 +1,86 @@
-const express =  require('express')
-const User =  require('../models/user')
-const router =  new express.Router()
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
 
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
 
-router.post('/users', async(request,res)=>{
-    const user = new User(request.body)
-    try{
+    try {
         await user.save()
-        res.status(201).send(user)
-    }catch(e){
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
         res.status(400).send(e)
     }
-    
-    // user.save().then(()=>{
-    //    res.status(201).send(user);
-    // }).catch((e)=>{
-    //     res.status(500).send();
-    // })
 })
 
-router.get('/users',async(request,res)=>{
-    try{
-        const users = await User.find({})
-        res.send(users)
-    }catch(e){
-        res.status(500).send()
-    }    
-    // User.find({}).then((users)=>{
-    //    res.status(201).send(users);
-    // }).catch((e)=>{
-    //     res.status(400).send(e);
-    // })
-})
-
-
-router.get('/users/:id',async(request,res)=>{
-   const _id = request.params.id
-   try{
-     const user = await User.findById(_id)
-     if(!user){
-        return res.status(404).send();
-      }
-      res.send(user)
-    }catch(e){
-     res.status(400).send()
-   }
-   
-})
-
-
-router.patch('/users/:id',async(request,res)=>{  
-    const updates = Object.keys(request.body)
-    const allowedUpdates = ['name','email','password','age']
-    const isValidOperation =  updates.every((update)=>{
-            return allowedUpdates.includes(update)
-    })
-    if(!isValidOperation){
-       return res.status(400).send({error:'Invalid updates!'}) 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
     }
-    try{
-        const user = await User.findById(request.params.id)
-        updates.forEach((update)=>{
-              user[update]=request.body[update]
-        })
-        await user.save() // this where middleware runs before this happens
-        if(!user){
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+
+        if (!user) {
             return res.status(404).send()
         }
+
         res.send(user)
-    }catch(e){
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
         res.status(400).send(e)
     }
 })
 
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
 
-router.delete('/users/:id',async(request,res)=>{
-    try{
-      const user = await User.findByIdAndDelete(request.params.id)
-      if(!user){
-          return res.status(404).send()
-      }
-      res.send(user)
-    }catch(e){
-       res.status(500).send() 
-    }
-})
+        if (!user) {
+            return res.status(404).send()
+        }
 
-router.post('/users/login',async(req,res)=>{
-    try{
-      const user = await User.findByCredentials(req.body.email,req.body.password)
-      res.send(user)
-    }catch(e){
-        res.status(400).send() 
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
